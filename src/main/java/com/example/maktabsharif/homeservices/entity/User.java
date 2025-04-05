bc72a683d0faa8d77af8ff8b77ae818151fe33cc
mostafa <mostafakhanderoo7@gmail.com>
2025-04-05 17:59:03 +0330

    validation added field

--- src/main/java/com/example/maktabsharif/homeservices/entity/User.java
+++ src/main/java/com/example/maktabsharif/homeservices/entity/User.java
@@ -1,4 +1,35 @@
 package com.example.maktabsharif.homeservices.entity;
 
-public class User {
+import com.example.maktabsharif.homeservices.entity.base.BaseEntity;
+import com.example.maktabsharif.homeservices.enumeration.Role;
+import com.example.maktabsharif.homeservices.enumeration.UserStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "users")
+public class User extends BaseEntity<Long> {
+    private String firstname;
+    private String lastname;
+    private Long age;
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+    private String password;
+    private String email;
+    private LocalDateTime registerDate;
+    @Lob
+    private byte[] userImage;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+
 }
