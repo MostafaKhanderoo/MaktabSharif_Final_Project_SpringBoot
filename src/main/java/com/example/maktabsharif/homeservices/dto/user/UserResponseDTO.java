38aa4eb2c753b57abb06492b57b3a72809ec9907
mostafa <mostafakhanderoo7@gmail.com>
2025-04-06 21:22:36 +0330

    add annotation builder

---
+++ src/main/java/com/example/maktabsharif/homeservices/dto/user/UserResponseDTO.java
@@ -0,0 +1,35 @@
+package com.example.maktabsharif.homeservices.dto.user;
+
+import com.example.maktabsharif.homeservices.entity.User;
+import com.example.maktabsharif.homeservices.enumeration.Role;
+import com.example.maktabsharif.homeservices.enumeration.UserStatus;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record UserResponseDTO(String firstName,
+                              String lastName,
+                              Long age,
+                              String username,
+                              String password,
+                              String email,
+                              LocalDateTime registerDate,
+                              Role role,
+                              UserStatus userStatus
+) {
+
+    public  static UserResponseDTO from(User user){
+        return  new UserResponseDTO(
+                user.getFirstname(),
+                user.getLastname(),
+                user.getAge(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getEmail(),
+                user.getRegisterDate(),
+                user.getRole(),
+                user.getUserStatus()
+        );
+    }
+}
