78aa1bf2957adc6b3566738340952735fc54d927
mostafa <mostafakhanderoo7@gmail.com>
2025-04-06 21:22:58 +0330

    record create user dto

---
+++ src/main/java/com/example/maktabsharif/homeservices/dto/user/UserCreateDTO.java
@@ -0,0 +1,26 @@
+package com.example.maktabsharif.homeservices.dto.user;
+
+import com.example.maktabsharif.homeservices.enumeration.Role;
+import com.example.maktabsharif.homeservices.enumeration.UserStatus;
+import lombok.Builder;
+
+import java.io.File;
+import java.time.LocalDateTime;
+
+
+@Builder
+public record UserCreateDTO(
+        String firstname,
+        String lastname,
+        Long age,
+        String username,
+        String password,
+        String email,
+        LocalDateTime registerDate,
+        String userImage,
+        Role role,
+        UserStatus userStatus
+
+) {
+}
+
