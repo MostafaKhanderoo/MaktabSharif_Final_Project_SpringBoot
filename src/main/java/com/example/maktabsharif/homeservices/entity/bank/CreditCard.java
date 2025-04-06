063223a83e15637575292094aa83693c6ce0b586
mostafa <mostafakhanderoo7@gmail.com>
2025-04-06 21:25:16 +0330

    credit user used for validity

--- src/main/java/com/example/maktabsharif/homeservices/entity/bank/CreditCard.java
+++ src/main/java/com/example/maktabsharif/homeservices/entity/bank/CreditCard.java
@@ -1,4 +1,33 @@
 package com.example.maktabsharif.homeservices.entity.bank;
 
-public class CreditCard {
+import com.example.maktabsharif.homeservices.entity.User;
+import com.example.maktabsharif.homeservices.entity.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CreditCard extends BaseEntity<Long> {
+    @Column(nullable = false,unique = true)
+    private String cardNumber;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String CVV2;
+    @Column(nullable = false)
+    private LocalDateTime dataCard;
+    @ManyToOne
+    private User user;
+    @ManyToOne
+    private Account account;
 }
