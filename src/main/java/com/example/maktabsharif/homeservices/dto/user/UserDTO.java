c1e01ef6ecc1243970333df293720fe88639c8b0
mostafa <mostafakhanderoo7@gmail.com>
2025-04-06 21:19:51 +0330

    refactor user return object

---
+++ src/main/java/com/example/maktabsharif/homeservices/dto/user/UserDTO.java
@@ -0,0 +1,27 @@
+package com.example.maktabsharif.homeservices.dto.user;
+
+import com.example.maktabsharif.homeservices.enumeration.Role;
+import com.example.maktabsharif.homeservices.enumeration.UserStatus;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.File;
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+public class UserDTO {
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private Long age;
+    private String username;
+    private String password;
+    private String email;
+    private LocalDateTime registerDate;
+    private String userImage;
+    private Role role;
+    private UserStatus userStatus;
+
+
+}
