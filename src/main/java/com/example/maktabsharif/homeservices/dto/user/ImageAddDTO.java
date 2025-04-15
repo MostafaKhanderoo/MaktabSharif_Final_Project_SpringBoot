2ef2b747cff2dc8239652f60ad741abdfd6fdd85
mostafa <mostafakhanderoo7@gmail.com>
2025-04-15 15:44:44 +0330

    add image in update profile user

---
+++ src/main/java/com/example/maktabsharif/homeservices/dto/user/ImageAddDTO.java
@@ -0,0 +1,9 @@
+package com.example.maktabsharif.homeservices.dto.user;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class ImageAddDTO {
+private MultipartFile image;
+}
