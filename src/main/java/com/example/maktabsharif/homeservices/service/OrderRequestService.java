e6600eca115a432ed71414eb9723420de7eabc19
mostafa <mostafakhanderoo7@gmail.com>
2025-04-15 15:58:03 +0330

    refactor crud and check order

--- src/main/java/com/example/maktabsharif/homeservices/service/OrderRequestService.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/OrderRequestService.java
@@ -1,4 +1,19 @@
 package com.example.maktabsharif.homeservices.service;
 
+import com.example.maktabsharif.homeservices.dto.order.OrderRequestCreateDTO;
+import com.example.maktabsharif.homeservices.dto.order.OrderRequestDTO;
+import com.example.maktabsharif.homeservices.entity.OrderRequest;
+import com.example.maktabsharif.homeservices.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface OrderRequestService {
+    OrderRequestDTO specialistRequestForOrder(OrderRequestCreateDTO orderRequestDTO);
+
+    Optional<OrderRequestDTO> existsOrderRequestByOrderIdAndSpecialistId(Long orderId,Long specialistId);
+
+     List<User> listCustomerRequest(Long customerRequests);
+
+    List<OrderRequestDTO> listSpecialistRequestForOrder(Long userId,Long orderId);
 }
