668d2ce16eaac111e08f3564e952cbaa7bb7ac3e
mostafa <mostafakhanderoo7@gmail.com>
2025-04-15 16:01:51 +0330

    dto return for service and subService

--- src/main/java/com/example/maktabsharif/homeservices/service/impl/ServiceManagementImpl.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/impl/ServiceManagementImpl.java
@@ -1,5 +1,8 @@
 package com.example.maktabsharif.homeservices.service.impl;
 
+import com.example.maktabsharif.homeservices.dto.servicedto.ServicesDTO;
+import com.example.maktabsharif.homeservices.dto.servicedto.SubServiceCreateDTO;
+import com.example.maktabsharif.homeservices.dto.servicedto.SubServiceDTO;
 import com.example.maktabsharif.homeservices.entity.ServiceEntity;
 import com.example.maktabsharif.homeservices.entity.SubService;
 import com.example.maktabsharif.homeservices.exception.CustomApiExceptionType;
@@ -26,30 +29,33 @@ public class ServiceManagementImpl implements ServiceManagementService {
     private final SubServiceRepository subServiceRepository;
 
     @Override
-    public void createService(String serviceName) {
+    public ServicesDTO createService(String serviceName) {
         if (serviceRepository.findByName(serviceName).isPresent())
             throw new ExistsException("service with name " +serviceName+ " alReady exists!",
                     CustomApiExceptionType.UNPROCESSIBLE_ENTITY);
 
         ServiceEntity serviceEntity = new ServiceEntity(serviceName);
         serviceRepository.save(serviceEntity);
-
+        log.info("service with name{"+serviceName+"} added");
+        return getDtoFromService(serviceEntity);
     }
 
 
     @Override
-    public void createSubService(String serviceName, String subServiceName, Double price, String description) {
-        var service=  serviceRepository.findByName(serviceName)
+    public SubServiceDTO createSubService(SubServiceCreateDTO createDTO) {
+        var service=  serviceRepository.findByName(createDTO.service().getName())
                 .orElseThrow(()->new  IllegalArgumentException("service not found!"));
 
-        if (subServiceRepository.existsByNameAndService(subServiceName,service))
-            throw new ExistsException(subServiceName+" subService alReady Exists for "+service.getName(),
+        if (subServiceRepository.existsByNameAndService(createDTO.name(),service))
+            throw new ExistsException(createDTO.name()+" subService alReady Exists for "+service.getName(),
                     CustomApiExceptionType.UNPROCESSIBLE_ENTITY);
 
 
-        SubService subService = new SubService(subServiceName, price, description);
+        SubService subService = new SubService(createDTO.name(), createDTO.basePrice(), createDTO.description());
         service.addSubService(subService);
         subServiceRepository.save(subService);
+        log.info("subService with name{"+createDTO.name()+"} added");
+        return getDtoFromSubService(subService);
     }
 
     @Override
@@ -59,5 +65,31 @@ public class ServiceManagementImpl implements ServiceManagementService {
             throw new NotFoundException("Service "+name+" not exists",CustomApiExceptionType.NOT_FOUND);
         return service.get();
 
+    }
+
+    @Override
+    public SubService findSubServiceById(Long id) {
+        Optional<SubService> service =subServiceRepository.findById(id);
+        if (service.isEmpty())
+            throw new NotFoundException("Service "+id+" not exists",CustomApiExceptionType.NOT_FOUND);
+        return service.get();
+    }
+
+    private ServicesDTO getDtoFromService (ServiceEntity service){
+        return ServicesDTO.builder()
+                .id(service.getId())
+                .name(service.getName())
+                .subServices(service.getSubServices())
+                .build();
+    }
+
+    private SubServiceDTO getDtoFromSubService(SubService subService){
+        return SubServiceDTO.builder()
+                .id(subService.getId())
+                .name(subService.getName())
+                .service(subService.getService())
+                .basePrice(subService.getBasePrice())
+                .description(subService.getDescription())
+                .build();
     }
 }
