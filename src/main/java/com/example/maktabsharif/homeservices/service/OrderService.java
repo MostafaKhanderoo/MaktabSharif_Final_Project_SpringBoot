89cd112f145eaa8b29983028b9d9bacf1141a289
mostafa <mostafakhanderoo7@gmail.com>
2025-04-15 15:58:34 +0330

    debug find order by id

--- src/main/java/com/example/maktabsharif/homeservices/service/OrderService.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/OrderService.java
@@ -3,11 +3,17 @@ package com.example.maktabsharif.homeservices.service;
 import com.example.maktabsharif.homeservices.dto.order.OrderCreateDTO;
 import com.example.maktabsharif.homeservices.dto.order.OrderDTO;
 import com.example.maktabsharif.homeservices.dto.order.OrderUpdateDTO;
+import com.example.maktabsharif.homeservices.entity.Orders;
+import com.example.maktabsharif.homeservices.entity.User;
+
+import java.util.Optional;
 
 public interface OrderService {
 
     OrderDTO addNewOrder(OrderCreateDTO createDTO);
     OrderDTO updateOrder(OrderUpdateDTO updateDTO);
+    Optional<Orders> findById(Long id);
     OrderDTO findOrderById(Long id);
     void  deleteOrderById(Long id);
+
 }
