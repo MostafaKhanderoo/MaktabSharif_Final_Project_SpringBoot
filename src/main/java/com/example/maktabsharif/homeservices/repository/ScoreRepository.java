b43e0491e2cf598bb250fc21241dfec733b9ec82
mostafa <mostafakhanderoo7@gmail.com>
2025-04-28 09:31:54 +0330

    methode find Score by order ,every order should one score set

--- src/main/java/com/example/maktabsharif/homeservices/repository/ScoreRepository.java
+++ src/main/java/com/example/maktabsharif/homeservices/repository/ScoreRepository.java
@@ -1,9 +1,16 @@
 package com.example.maktabsharif.homeservices.repository;
 
+import com.example.maktabsharif.homeservices.dto.score.ScoreDto;
 import com.example.maktabsharif.homeservices.entity.Score;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ScoreRepository extends JpaRepository<Score,Long> {
+    @Query("SELECT c from Score c where c.orderId.id =:id")
+    Optional<ScoreDto> findScoreByOrderId(@Param("id") Long id);
 }
