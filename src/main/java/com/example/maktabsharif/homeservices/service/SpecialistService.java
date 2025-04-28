0eba372abf9fe9f3a4a0941a66b10816bb027ebd
mostafa <mostafakhanderoo7@gmail.com>
2025-04-28 10:10:29 +0330

    debug save and update specialist to IoException

--- src/main/java/com/example/maktabsharif/homeservices/service/SpecialistService.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/SpecialistService.java
@@ -5,9 +5,11 @@ import com.example.maktabsharif.homeservices.dto.user.UserDTO;
 import com.example.maktabsharif.homeservices.dto.user.UserUpdateDTO;
 import com.example.maktabsharif.homeservices.entity.User;
 
+import java.io.IOException;
+
 public interface SpecialistService {
-    UserDTO savaSpecialist(UserCreateDTO createDTO);
-    UserDTO updateSpecialist(UserUpdateDTO updateDTO);
+    UserDTO savaSpecialist(UserCreateDTO createDTO) throws IOException;
+    UserDTO updateSpecialist(UserUpdateDTO updateDTO) throws IOException;
     UserDTO findById(Long id);
     void deleteById(Long id);
     User findByIdUser(Long id);
