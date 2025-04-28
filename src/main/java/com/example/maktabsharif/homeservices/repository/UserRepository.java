c25a6cf6fa54c0a3ecef53be1e5a620b62c44f42
mostafa <mostafakhanderoo7@gmail.com>
2025-04-28 10:09:19 +0330

    JpaSpecificationExecutor for search and find users list

--- src/main/java/com/example/maktabsharif/homeservices/repository/UserRepository.java
+++ src/main/java/com/example/maktabsharif/homeservices/repository/UserRepository.java
@@ -3,13 +3,14 @@ package com.example.maktabsharif.homeservices.repository;
 import com.example.maktabsharif.homeservices.entity.User;
 import com.example.maktabsharif.homeservices.enumeration.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long> , JpaSpecificationExecutor<User> {
 
     Optional<User> findUserById(Long id);
     Boolean existsUserByUsernameAndRole(String username, Role role);
@@ -19,4 +20,8 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
 
     User findUserByRoleAndFirstname(Role role ,String firstName);
+
+    Optional<User>findByEmail(String email);
+    List<User> findAllByRole(Role role);
+
 }
