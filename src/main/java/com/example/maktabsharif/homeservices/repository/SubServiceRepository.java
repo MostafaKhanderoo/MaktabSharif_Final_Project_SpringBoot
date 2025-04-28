b943e464cdb541b8cf5ccc76b4393f27f3d9d985
mostafa <mostafakhanderoo7@gmail.com>
2025-04-28 09:32:27 +0330

    find  subService by service name

--- src/main/java/com/example/maktabsharif/homeservices/repository/SubServiceRepository.java
+++ src/main/java/com/example/maktabsharif/homeservices/repository/SubServiceRepository.java
@@ -3,12 +3,17 @@ package com.example.maktabsharif.homeservices.repository;
 import com.example.maktabsharif.homeservices.entity.ServiceEntity;
 import com.example.maktabsharif.homeservices.entity.SubService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SubServiceRepository extends JpaRepository<SubService,Long> {
     Optional<SubService> findByName(String name);
     boolean existsByNameAndService(String name, ServiceEntity service);
+    @Query("select s from SubService s where s.service.name = :serviceName")
+    List<SubService> findAllByServiceName(@Param("serviceName") String serviceName);
 }
