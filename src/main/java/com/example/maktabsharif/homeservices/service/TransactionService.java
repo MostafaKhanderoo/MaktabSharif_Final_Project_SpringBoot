289c5b30278e19ab532d6f51d2379f7b1fd47f6e
mostafa <mostafakhanderoo7@gmail.com>
2025-04-28 10:09:43 +0330

    create and update transaction

--- src/main/java/com/example/maktabsharif/homeservices/service/TransactionService.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/TransactionService.java
@@ -1,4 +1,11 @@
 package com.example.maktabsharif.homeservices.service;
 
+import com.example.maktabsharif.homeservices.dto.transactionorder.TransactionCreateDTO;
+import com.example.maktabsharif.homeservices.dto.transactionorder.TransactionDTO;
+
+import java.util.Optional;
+
 public interface TransactionService {
+    TransactionDTO createTransaction(TransactionCreateDTO transactionCreateDTO);
+    Optional<TransactionDTO>findTransactionById(Long id);
 }
