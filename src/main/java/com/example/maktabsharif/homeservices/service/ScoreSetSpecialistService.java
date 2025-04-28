1971bcfad5d4b318492db25e32ea81e796927d83
mostafa <mostafakhanderoo7@gmail.com>
2025-04-28 10:32:43 +0330

    debug crud for score

--- src/main/java/com/example/maktabsharif/homeservices/service/ScoreSetSpecialistService.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/ScoreSetSpecialistService.java
@@ -1,4 +1,22 @@
 package com.example.maktabsharif.homeservices.service;
 
+import com.example.maktabsharif.homeservices.dto.score.ScoreCreateDTO;
+import com.example.maktabsharif.homeservices.dto.score.ScoreDto;
+import com.example.maktabsharif.homeservices.dto.score.ScoreUpdateDTO;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ScoreSetSpecialistService {
+
+    ScoreDto addScoreSpecialist(ScoreCreateDTO scoreCreateDTO);
+    ScoreDto updateScore(ScoreUpdateDTO scoreUpdateDTO);
+    Optional<ScoreDto> findDtoById(Long id);
+    List<ScoreDto> findAllScore();
+    void deleteById(Long id);
+    Optional<ScoreDto> findBySpecialistId(Long id);
+    List<ScoreDto> specialistSetScore(Long specialistId);
+    void checkOrderHasScore(Long id);
+
+
 }
