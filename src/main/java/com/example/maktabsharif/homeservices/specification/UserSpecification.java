627976645f69d5b7600a90b6f173397ae6914603
mostafa <mostafakhanderoo7@gmail.com>
2025-04-28 11:05:35 +0330

    user specification for search user by age and value

---
+++ src/main/java/com/example/maktabsharif/homeservices/specification/UserSpecification.java
@@ -0,0 +1,32 @@
+package com.example.maktabsharif.homeservices.specification;
+
+import com.example.maktabsharif.homeservices.dto.user.UserDTO;
+import com.example.maktabsharif.homeservices.entity.User;
+import com.example.maktabsharif.homeservices.enumeration.Operator;
+import org.springframework.data.jpa.domain.Specification;
+
+public class UserSpecification {
+
+    public static Specification<User> filterByString(String field, String value) {
+        return ((root, query, cb) ->{
+            if (field ==null || value ==null){
+
+               return cb.disjunction();
+            }
+            return cb.like(root.get(field),value);
+        }
+        );
+
+    }
+    public static Specification<User> filterByLong(Long value , Operator operator){
+        return ((root, query, cb) ->switch (operator){
+            case GT -> cb.greaterThan(root.get("age"),value);
+            case LT -> cb.lessThan(root.get("age"),value);
+            case EQ -> cb.equal(root.get("age"),value);
+            case GTQ -> cb.greaterThanOrEqualTo(root.get("age"),value);
+            case LTQ -> cb.lessThanOrEqualTo(root.get("age"),value);
+
+        }
+        );
+    }
+}
