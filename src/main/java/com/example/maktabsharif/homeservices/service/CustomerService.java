b3dad0670201b1dc38e212a9f31557ff7ad02dee
mostafa <mostafakhanderoo7@gmail.com>
2025-04-28 10:33:06 +0330

    refactor save and update user

--- src/main/java/com/example/maktabsharif/homeservices/service/CustomerService.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/CustomerService.java
@@ -4,11 +4,15 @@ import com.example.maktabsharif.homeservices.dto.user.UserCreateDTO;
 import com.example.maktabsharif.homeservices.dto.user.UserDTO;
 import com.example.maktabsharif.homeservices.dto.user.UserUpdateDTO;
 import com.example.maktabsharif.homeservices.entity.User;
+import com.example.maktabsharif.homeservices.enumeration.Operator;
+
+import java.io.IOException;
+import java.util.List;
 
 public interface CustomerService {
-    UserDTO savaCustomer(UserCreateDTO createDTO);
+    UserDTO savaCustomer(UserCreateDTO createDTO) throws IOException;
 
-    UserDTO updateCustomer(UserUpdateDTO updateDTO);
+    UserDTO updateCustomer(UserUpdateDTO updateDTO) throws IOException;
 
     UserDTO findByIdAndRole(Long id);
 
@@ -17,4 +21,14 @@ public interface CustomerService {
     User chooseLoginCustomerById(Long id);
 
     User findUserByFirstName(String firstName);
+
+    void  changePassword (Long userId,String currentPassword,String newPassword);
+
+
+    List<UserDTO> searchUserByAge(Long age, Operator operator);
+
+    List<UserDTO> searchUserByFieldAndValue(String field,String value);
+
+
+    List<UserDTO> getAllUser();
 }
