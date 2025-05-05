b23388c43346b97e78b5fcb7ed50ec9ccf836c27
mostafa <mostafakhanderoo7@gmail.com>
2025-05-06 00:44:04 +0330

    add methods for get order by customer and list of order

--- src/main/java/com/example/maktabsharif/homeservices/repository/OrdersRepository.java
+++ src/main/java/com/example/maktabsharif/homeservices/repository/OrdersRepository.java
@@ -2,8 +2,16 @@ package com.example.maktabsharif.homeservices.repository;
 
 import com.example.maktabsharif.homeservices.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrdersRepository extends JpaRepository<Orders,Long> {
+    @Query("select o from Orders  o where o.specialistAccepted is null")
+    List<Orders> getListOrderSpecialistNull();
+
+    List<Orders>getOrdersByCustomerRequestServiceId(Long customerRequestedId);
+
 }
