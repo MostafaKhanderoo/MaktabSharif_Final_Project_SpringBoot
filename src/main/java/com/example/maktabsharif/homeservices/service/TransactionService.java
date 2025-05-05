fe66e85084f96857f4be10aa64a6b60794e954fd
mostafa <mostafakhanderoo7@gmail.com>
2025-05-06 00:49:52 +0330

    methode get transaction by id

--- src/main/java/com/example/maktabsharif/homeservices/service/TransactionService.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/TransactionService.java
@@ -2,10 +2,12 @@ package com.example.maktabsharif.homeservices.service;
 
 import com.example.maktabsharif.homeservices.dto.transactionorder.TransactionCreateDTO;
 import com.example.maktabsharif.homeservices.dto.transactionorder.TransactionDTO;
+import com.example.maktabsharif.homeservices.entity.Transaction;
 
 import java.util.Optional;
 
 public interface TransactionService {
     TransactionDTO createTransaction(TransactionCreateDTO transactionCreateDTO);
     Optional<TransactionDTO>findTransactionById(Long id);
+    Transaction findById(Long id);
 }
