ed8b7a0cf82dcaf6804569b9e2115a01831a8242
mostafa <mostafakhanderoo7@gmail.com>
2025-05-06 00:48:10 +0330

    rename methode for customer choose specialist

--- src/main/java/com/example/maktabsharif/homeservices/service/OrderService.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/OrderService.java
@@ -2,10 +2,13 @@ package com.example.maktabsharif.homeservices.service;
 
 import com.example.maktabsharif.homeservices.dto.order.OrderCreateDTO;
 import com.example.maktabsharif.homeservices.dto.order.OrderDTO;
+import com.example.maktabsharif.homeservices.dto.order.OrderResponseDTO;
 import com.example.maktabsharif.homeservices.dto.order.OrderUpdateDTO;
 import com.example.maktabsharif.homeservices.entity.Orders;
 import com.example.maktabsharif.homeservices.entity.User;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderService {
@@ -15,5 +18,9 @@ public interface OrderService {
     Optional<Orders> findById(Long id);
     OrderDTO findOrderById(Long id);
     void  deleteOrderById(Long id);
+    List<OrderResponseDTO> getAllNullSpecialistOrder();
 
+    List<OrderResponseDTO>ordersOfCustomer(Long customerRequestedId);
+
+    OrderDTO customerChooseSpecialist(Long orderId,Long specialistId);
 }
