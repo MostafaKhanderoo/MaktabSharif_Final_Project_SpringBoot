836a50574c099fe3427e04a99a351afd03b36dc5
mostafa <mostafakhanderoo7@gmail.com>
2025-05-05 23:38:33 +0330

    refactor orderStatus for create set in services

--- src/main/java/com/example/maktabsharif/homeservices/dto/order/OrderCreateDTO.java
+++ src/main/java/com/example/maktabsharif/homeservices/dto/order/OrderCreateDTO.java
@@ -1,15 +1,13 @@
 package com.example.maktabsharif.homeservices.dto.order;
 
-import com.example.maktabsharif.homeservices.enumeration.OrderStatus;
-
 public record OrderCreateDTO(
         String subService,
 
         Long customerRequestService,
 
-        double orderPriceRequest,
+        double orderPriceRequest
 
 
-        OrderStatus orderStatus
+
 ) {
 }
