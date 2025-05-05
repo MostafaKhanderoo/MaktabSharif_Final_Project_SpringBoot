654de5a6a262d8de8539f16f2516b5a4cd0a61b7
mostafa <mostafakhanderoo7@gmail.com>
2025-05-05 23:03:14 +0330

    debug and find list orders

--- src/main/java/com/example/maktabsharif/homeservices/controller/OrderController.java
+++ src/main/java/com/example/maktabsharif/homeservices/controller/OrderController.java
@@ -2,14 +2,14 @@ package com.example.maktabsharif.homeservices.controller;
 
 import com.example.maktabsharif.homeservices.dto.order.OrderCreateDTO;
 import com.example.maktabsharif.homeservices.dto.order.OrderDTO;
-import com.example.maktabsharif.homeservices.entity.User;
+import com.example.maktabsharif.homeservices.dto.order.OrderResponseDTO;
 import com.example.maktabsharif.homeservices.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/order")
@@ -20,7 +20,25 @@ public class OrderController {
 
     @PostMapping("/create")
     public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderCreateDTO createDTO){
-      return   ResponseEntity
+        return   ResponseEntity
                 .ok(orderService.addNewOrder(createDTO));
     }
+
+    @GetMapping("/order/list")
+    public ResponseEntity<List<OrderResponseDTO>> getListNullSpecialist(){
+        return ResponseEntity.ok(orderService.getAllNullSpecialistOrder());
+    }
+
+    @GetMapping("/list/customer/order/{customerId}")
+    public ResponseEntity<List<OrderResponseDTO>> getCustomerOrders(@PathVariable Long customerId){
+       return ResponseEntity.ok(orderService.ordersOfCustomer(customerId));
+    }
+
+    @PostMapping("/accept/specialist/request")
+    public ResponseEntity<OrderDTO> acceptSpecialistRequest(
+            @RequestParam Long orderId,
+            @RequestParam Long specialistId) {
+        return ResponseEntity.ok(orderService.customerChooseSpecialist(orderId, specialistId));
+    }
+
 }
