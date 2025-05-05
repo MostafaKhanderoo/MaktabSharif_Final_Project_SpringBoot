b2acbb552c1858313459b9104bae6b5fc804c9ba
mostafa <mostafakhanderoo7@gmail.com>
2025-05-06 00:51:13 +0330

    save transaction validity handle

--- src/main/java/com/example/maktabsharif/homeservices/service/ValidityTransactionService.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/ValidityTransactionService.java
@@ -1,4 +1,8 @@
 package com.example.maktabsharif.homeservices.service;
 
+import com.example.maktabsharif.homeservices.entity.ValidityTransaction;
+
 public interface ValidityTransactionService {
+
+    ValidityTransaction saveValidityTransaction(Long orderId, Long transactionId);
 }
