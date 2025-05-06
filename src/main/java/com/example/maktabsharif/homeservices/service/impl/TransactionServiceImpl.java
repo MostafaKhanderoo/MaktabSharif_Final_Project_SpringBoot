80436ed80b148c133195d74f8ed11b98209360c6
mostafa <mostafakhanderoo7@gmail.com>
2025-05-06 13:16:11 +0330

    refactor method find by id

--- src/main/java/com/example/maktabsharif/homeservices/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/impl/TransactionServiceImpl.java
@@ -3,8 +3,12 @@ package com.example.maktabsharif.homeservices.service.impl;
 import com.example.maktabsharif.homeservices.dto.transactionorder.TransactionCreateDTO;
 import com.example.maktabsharif.homeservices.dto.transactionorder.TransactionDTO;
 import com.example.maktabsharif.homeservices.entity.Transaction;
+import com.example.maktabsharif.homeservices.exception.CustomApiExceptionType;
+import com.example.maktabsharif.homeservices.exception.NotFoundException;
 import com.example.maktabsharif.homeservices.repository.TransactionRepository;
+import com.example.maktabsharif.homeservices.service.OrderService;
 import com.example.maktabsharif.homeservices.service.TransactionService;
+import com.example.maktabsharif.homeservices.service.ValidityTransactionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,7 +22,8 @@ import java.util.Optional;
 public class TransactionServiceImpl implements TransactionService {
 
     private final TransactionRepository transactionRepository;
-
+   // private final ValidityTransactionService validityTransactionService;
+    private final OrderService orderService;
     @Override
     public TransactionDTO createTransaction(TransactionCreateDTO transactionCreateDTO) {
 
@@ -26,17 +31,28 @@ public class TransactionServiceImpl implements TransactionService {
         transaction.setAmount(transactionCreateDTO.amount());
         transaction.setTimeTransaction(LocalDateTime.now());
         transaction.setCustomerValidity(transactionCreateDTO.customerValidity());
-        transaction.setSpecialistValidity(transactionCreateDTO.SpecialistValidity());
+        transaction.setSpecialistValidity(transactionCreateDTO.specialistValidity());
+       var orderSelected= orderService.findById(transactionCreateDTO.orderSelected()).get();
+        transaction.setOrders(orderSelected);
 
-        transactionRepository.save(transaction);
+       var transactionCreated= transactionRepository.save(transaction);
+   //    validityTransactionService.saveValidityTransaction(transactionCreateDTO.orderId(),transactionCreated.getId());
         log.info("Transaction with id{"+transaction.getId()+"} saved!");
-        return getTransaction(transaction);
+        return getTransaction(transactionCreated);
 
     }
 
     @Override
     public Optional<TransactionDTO> findTransactionById(Long id) {
         return Optional.empty();
+    }
+
+    @Override
+    public Transaction findById(Long id) {
+         if(transactionRepository.findById(id).isEmpty())
+             throw new NotFoundException("Transaction with id{"+id+"} not found",
+                     CustomApiExceptionType.NOT_FOUND);
+         return transactionRepository.findById(id).get();
     }
 
     private TransactionDTO getTransaction(Transaction transaction){
