481d7890b98e208d39b17e1fc12972066909777a
mostafa <mostafakhanderoo7@gmail.com>
2025-05-06 13:17:26 +0330

    cheat validity transaction

--- src/main/java/com/example/maktabsharif/homeservices/service/impl/ValidityTransactionServiceImpl.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/impl/ValidityTransactionServiceImpl.java
@@ -1,6 +1,11 @@
 package com.example.maktabsharif.homeservices.service.impl;
 
+import com.example.maktabsharif.homeservices.entity.Orders;
+import com.example.maktabsharif.homeservices.entity.Transaction;
+import com.example.maktabsharif.homeservices.entity.ValidityTransaction;
 import com.example.maktabsharif.homeservices.repository.ValidityTransactionRepository;
+import com.example.maktabsharif.homeservices.service.OrderService;
+import com.example.maktabsharif.homeservices.service.TransactionService;
 import com.example.maktabsharif.homeservices.service.ValidityTransactionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,5 +17,21 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class ValidityTransactionServiceImpl implements ValidityTransactionService {
-    private  final ValidityTransactionRepository validityTransactionRepository;
+    private final ValidityTransactionRepository validityTransactionRepository;
+    private final OrderService orderService;
+    private final TransactionService transactionService;
+
+    @Override
+    public ValidityTransaction saveValidityTransaction(Long orderId, Long transactionId) {
+       Orders order= orderService.findById(orderId).get();
+        Transaction transaction = transactionService.findById(transactionId);
+        ValidityTransaction validityTransaction = new ValidityTransaction();
+        validityTransaction.setTransaction(transaction);
+        validityTransaction.setOrders(order);
+        log.info("Validity Transaction create for order id{"+orderId+"}" +
+                " and transaction id{"+transactionId+"}");
+
+      return validityTransactionRepository.save(validityTransaction);
+
+    }
 }
