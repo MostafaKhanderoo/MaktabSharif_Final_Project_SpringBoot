206053e87bb9a93c5efccecd160c0ca525a0a439
mostafa <mostafakhanderoo7@gmail.com>
2025-05-08 18:29:23 +0330

    get token username and password user

--- src/main/java/com/example/maktabsharif/homeservices/entity/User.java
+++ src/main/java/com/example/maktabsharif/homeservices/entity/User.java
@@ -1,22 +1,24 @@
 package com.example.maktabsharif.homeservices.entity;
 
 import com.example.maktabsharif.homeservices.entity.base.BaseEntity;
-import com.example.maktabsharif.homeservices.enumeration.Role;
 import com.example.maktabsharif.homeservices.enumeration.UserStatus;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Entity
+@Builder
 @Getter
 @Setter
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @Table(name = "users")
-public class User extends BaseEntity<Long> {
+public class User extends BaseEntity<Long> implements UserDetails {
     private String firstname;
     private String lastname;
     private Long age;
@@ -30,8 +32,23 @@ public class User extends BaseEntity<Long> {
     @Lob
     private byte[] userImage;
     @Enumerated(EnumType.STRING)
+    @ManyToOne
     private Role role;
     @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
 }
