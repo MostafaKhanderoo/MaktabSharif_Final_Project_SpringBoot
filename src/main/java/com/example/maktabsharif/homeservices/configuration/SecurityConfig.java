958fac93ae7f96bb094faf6f21296c69d32341ee
mostafa <mostafakhanderoo7@gmail.com>
2025-05-08 18:03:45 +0330

    config Security to project

---
+++ src/main/java/com/example/maktabsharif/homeservices/configuration/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.maktabsharif.homeservices.configuration;
+
+import com.example.maktabsharif.homeservices.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+            ,UserService userService
+    ) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize.
+                       requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                        .requestMatchers("/api/**").authenticated().anyRequest().denyAll())
+                .httpBasic(Customizer.withDefaults());
+
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/example/maktabsharif/homeservices/enumeration/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.maktabsharif.homeservices.enumeration;
-
-public enum Role {
-    CUSTOMER,
-    SPECIALIST,
-    MANAGER
-}
