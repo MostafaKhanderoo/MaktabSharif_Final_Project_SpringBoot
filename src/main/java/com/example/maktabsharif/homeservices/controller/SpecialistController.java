2f613570b0e65205232b546da157ad10500c47be
mostafa <mostafakhanderoo7@gmail.com>
2025-05-09 22:54:17 +0330

    refactor service user

--- src/main/java/com/example/maktabsharif/homeservices/controller/SpecialistController.java
+++ src/main/java/com/example/maktabsharif/homeservices/controller/SpecialistController.java
@@ -1,58 +1,61 @@
-package com.example.maktabsharif.homeservices.controller;
+    package com.example.maktabsharif.homeservices.controller;
 
-import com.example.maktabsharif.homeservices.dto.user.UserCreateDTO;
-import com.example.maktabsharif.homeservices.dto.user.UserDTO;
-import com.example.maktabsharif.homeservices.dto.user.UserUpdateDTO;
-import com.example.maktabsharif.homeservices.service.SpecialistService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+    import com.example.maktabsharif.homeservices.dto.user.UserCreateDTO;
+    import com.example.maktabsharif.homeservices.dto.user.UserDTO;
+    import com.example.maktabsharif.homeservices.dto.user.UserUpdateDTO;
+    import com.example.maktabsharif.homeservices.enumeration.RoleName;
+    import com.example.maktabsharif.homeservices.service.SpecialistService;
+    import com.example.maktabsharif.homeservices.service.UserService;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
+    import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+    import java.io.IOException;
 
-@RestController
-@RequestMapping("/api/specialist")
-@RequiredArgsConstructor
-public class SpecialistController {
+    @RestController
+    @RequestMapping("/api/specialist")
+    @RequiredArgsConstructor
+    public class SpecialistController {
 
-    private  final SpecialistService specialistService;
+        private  final SpecialistService specialistService;
+        private final UserService userService;
 
-    @PostMapping("/create")
-    public ResponseEntity<UserDTO> createSpecialist(
-            @RequestParam MultipartFile profileImage,
-            @RequestParam String firstname,
-            @RequestParam String lastname,
-            @RequestParam Long age,
-            @RequestParam String username,
-            @RequestParam String password,
-            @RequestParam String email
-    ) throws IOException {
-        UserCreateDTO createDTO = new UserCreateDTO(firstname,lastname,age,username,password,email,profileImage);
-        return ResponseEntity
-                .ok(specialistService.savaSpecialist(createDTO));
+        @PostMapping("/create")
+        public ResponseEntity<UserDTO> createSpecialist(
+                @RequestParam MultipartFile profileImage,
+                @RequestParam String firstname,
+                @RequestParam String lastname,
+                @RequestParam Long age,
+                @RequestParam String username,
+                @RequestParam String password,
+                @RequestParam String email
+        ) throws IOException {
+            UserCreateDTO createDTO = new UserCreateDTO(firstname,lastname,age,username,password,email,profileImage);
+            return ResponseEntity
+                    .ok(userService.saveUser(createDTO, RoleName.SPECIALIST));
+
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<UserDTO> findSpecialistById(@PathVariable Long id){
+            return ResponseEntity
+                    .ok(specialistService.findById(id));
+        }
+
+
+        @PutMapping("/update")
+        public ResponseEntity<UserDTO> updateSpecialist(@RequestBody UserUpdateDTO updateDTO) throws IOException {
+            return ResponseEntity
+                    .ok(specialistService.updateSpecialist(updateDTO));
+        }
+        @DeleteMapping ("/{id}")
+        public ResponseEntity<Void> deleteSpecialist(@PathVariable Long id){
+            specialistService.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+
+
+
 
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<UserDTO> findSpecialistById(@PathVariable Long id){
-        return ResponseEntity
-                .ok(specialistService.findById(id));
-    }
-
-
-    @PutMapping("/update")
-    public ResponseEntity<UserDTO> updateSpecialist(@RequestBody UserUpdateDTO updateDTO) throws IOException {
-        return ResponseEntity
-                .ok(specialistService.updateSpecialist(updateDTO));
-    }
-    @DeleteMapping ("/{id}")
-    public ResponseEntity<Void> deleteSpecialist(@PathVariable Long id){
-        specialistService.deleteById(id);
-        return ResponseEntity.ok().build();
-    }
-
-
-
-
-}
