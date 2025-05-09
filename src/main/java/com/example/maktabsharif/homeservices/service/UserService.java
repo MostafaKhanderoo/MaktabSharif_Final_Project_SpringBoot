444be8862c2908fee718e15a68fcfacf513b1dda
mostafa <mostafakhanderoo7@gmail.com>
2025-05-09 22:57:57 +0330

    refactor details and add CRUD methods

--- src/main/java/com/example/maktabsharif/homeservices/service/UserService.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/UserService.java
@@ -1,8 +1,38 @@
 package com.example.maktabsharif.homeservices.service;
 
+import com.example.maktabsharif.homeservices.dto.user.UserCreateDTO;
+import com.example.maktabsharif.homeservices.dto.user.UserDTO;
+import com.example.maktabsharif.homeservices.dto.user.UserUpdateDTO;
 import com.example.maktabsharif.homeservices.entity.User;
+import com.example.maktabsharif.homeservices.enumeration.Operator;
+import com.example.maktabsharif.homeservices.enumeration.RoleName;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService extends UserDetailsService {
-    User save(User user);
+
+    UserDTO saveUser(UserCreateDTO createDTO, RoleName roleName)throws IOException;
+    UserDTO updateUser(UserUpdateDTO updateDTO) throws IOException;
+
+    UserDTO findByIdAndRole(Long id);
+
+    void deleteById(Long id);
+
+    Optional<User> chooseLoginUserById(Long id);
+
+    User findUserByFirstName(String firstName);
+
+    void  changePassword (Long userId,String currentPassword,String newPassword);
+
+
+    List<UserDTO> searchUserByAge(Long age, Operator operator);
+
+    List<UserDTO> searchUserByFieldAndValue(String field,String value);
+
+
+    List<UserDTO> getAllUser();
+
 }
