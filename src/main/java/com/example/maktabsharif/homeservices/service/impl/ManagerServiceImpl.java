0fb99f7a4f1d0abf88d85dab93631a0476e368d8
mostafa <mostafakhanderoo7@gmail.com>
2025-05-31 15:24:49 +0330

    refactor delete role to response data of manager

--- src/main/java/com/example/maktabsharif/homeservices/service/impl/ManagerServiceImpl.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/impl/ManagerServiceImpl.java
@@ -5,24 +5,19 @@ import com.example.maktabsharif.homeservices.dto.user.UserDTO;
 import com.example.maktabsharif.homeservices.dto.user.UserUpdateDTO;
 import com.example.maktabsharif.homeservices.entity.Role;
 import com.example.maktabsharif.homeservices.entity.User;
-import com.example.maktabsharif.homeservices.enumeration.RoleName;
 import com.example.maktabsharif.homeservices.enumeration.UserStatus;
 import com.example.maktabsharif.homeservices.exception.CustomApiExceptionType;
-import com.example.maktabsharif.homeservices.exception.ExistsException;
 import com.example.maktabsharif.homeservices.exception.InvalidInputException;
-import com.example.maktabsharif.homeservices.exception.NotFoundException;
-import com.example.maktabsharif.homeservices.repository.RoleRepository;
-import com.example.maktabsharif.homeservices.repository.UserRepository;
 import com.example.maktabsharif.homeservices.service.ManagerService;
+import com.example.maktabsharif.homeservices.service.RoleService;
+import com.example.maktabsharif.homeservices.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.Optional;
+import java.time.LocalDateTime ;
 
 @Service
 @RequiredArgsConstructor
@@ -30,46 +25,36 @@ import java.util.Optional;
 @Transactional
 public class ManagerServiceImpl implements ManagerService {
 
-    private final UserRepository managerRepository;
-    private final RoleRepository roleRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final UserService managerService;
+    private final RoleService roleService;
+
 
 
     @Override
     public UserDTO savaManager(UserCreateDTO createDTO) throws IOException {
-        if (managerRepository.existsUserByUsernameAndRole(createDTO.username(), RoleName.MANAGER))
-            throw new ExistsException("Manager with username {"
-                    + createDTO.username() + "} already exists!",
-                    CustomApiExceptionType.UNPROCESSIBLE_ENTITY);
-
         User user = new User();
+        user.setRegisterDate(LocalDateTime.now());
         user.setFirstname(createDTO.firstname());
         user.setLastname(createDTO.lastname());
-        user.setAge(createDTO.age());
         user.setUsername(createDTO.username());
-        user.setPassword(passwordEncoder.encode(createDTO.password()));
+        user.setPassword(createDTO.password());
+        user.setAge(createDTO.age());
         user.setEmail(createDTO.email());
-        user.setRegisterDate(LocalDateTime.now());
-
-        Role role =roleRepository.findByName(RoleName.MANAGER)
-                .orElseThrow(()->new NotFoundException(CustomApiExceptionType.NOT_FOUND));
-        user.setRole(role);
+        Role role =roleService.findByName(Role.ADMIN);
+        user.getRole().add(role);
         user.setUserStatus(UserStatus.PENDING);
-        user.setUserImage(createDTO.profileImage().getBytes());
 
-        var saveManager =managerRepository.save(user);
+
+        var saveManager =managerService.saveUser(user);
         log.info("Manager with id {} saved", saveManager.getId());
         return getDtoFromManager(saveManager);
     }
 
     @Override
     public UserDTO updateManager(UserUpdateDTO updateDTO) throws IOException {
-        if (managerRepository.findUserByIdAndRole(updateDTO.id(), RoleName.MANAGER).isEmpty())
-            throw new NotFoundException("Customer with id {"
-                    + updateDTO.id() + "} not found!",
-                    CustomApiExceptionType.NOT_FOUND);
 
-        User updateCustomer = managerRepository.findUserById(updateDTO.id()).get();
+
+        User updateCustomer =managerService.findById(updateDTO.id());
 
         updateCustomer.setFirstname(updateDTO.firstname());
         updateCustomer.setLastname(updateDTO.lastname());
@@ -80,42 +65,27 @@ public class ManagerServiceImpl implements ManagerService {
             throw new InvalidInputException("Age must be 18 or older!"
                     ,CustomApiExceptionType.UNPROCESSIBLE_ENTITY);
 
-        if (managerRepository.existsUserByUsernameAndRole(updateDTO.username(), RoleName.CUSTOMER))
-            throw new ExistsException("Manager with username {"
-                    + updateDTO.username() + "} already exists!",
-                    CustomApiExceptionType.UNPROCESSIBLE_ENTITY);
 
         updateCustomer.setUsername(updateDTO.username());
         updateCustomer.setEmail(updateDTO.email());
         updateCustomer.setPassword(updateDTO.password());
         updateCustomer.setUserImage(updateDTO.image().getBytes());
 
-        User savaManager =managerRepository.save(updateCustomer);
+        User savaManager =managerService.updateUser(updateCustomer);
         log.info("Manager with id {} updated", savaManager.getId());
         return getDtoFromManager(savaManager);
     }
 
     @Override
     public UserDTO findById(Long id) {
-        Optional<User> manager =managerRepository.findUserByIdAndRole(id , RoleName.MANAGER);
+      var manager=  managerService.findById(id);
 
-        if (manager.isEmpty())
-            throw new NotFoundException("Manager with id{"+
-                    id+"} not found!"
-                    ,CustomApiExceptionType.NOT_FOUND);
-
-        return getDtoFromManager(manager.get());
+        return getDtoFromManager(manager);
     }
 
     @Override
     public void deleteById(Long id) {
-        if(managerRepository.findUserByIdAndRole(id, RoleName.MANAGER).isEmpty())
-            throw new NotFoundException("Manager with id{"+
-                    id+"} not found!"
-                    ,CustomApiExceptionType.NOT_FOUND);
-
-        managerRepository.deleteById(id);
-
+       managerService.deleteById(id);
     }
 
     @Override
@@ -132,7 +102,6 @@ public class ManagerServiceImpl implements ManagerService {
                 .email(user.getEmail())
                 .password(user.getPassword())
                 .registerDate(user.getRegisterDate())
-                .role(user.getRole())
                 .profileImage(user.getUserImage())
                 .userStatus(user.getUserStatus())
                 .build();
