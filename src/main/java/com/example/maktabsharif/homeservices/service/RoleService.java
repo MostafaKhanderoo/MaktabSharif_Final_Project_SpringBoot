58700ea4346d80ebd5bbec5dede560c96067b798
mostafa <mostafakhanderoo7@gmail.com>
2025-05-31 19:36:46 +0330

    interface role service

---
+++ src/main/java/com/example/maktabsharif/homeservices/service/RoleService.java
@@ -0,0 +1,7 @@
+package com.example.maktabsharif.homeservices.service;
+
+import com.example.maktabsharif.homeservices.entity.Role;
+
+public interface RoleService {
+    Role findByName(String name);
+}
