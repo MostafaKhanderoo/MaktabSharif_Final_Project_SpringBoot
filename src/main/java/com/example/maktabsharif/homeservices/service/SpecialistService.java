da9ad1241bbdf14b273317236e67b65e6a68c7b5
mostafa <mostafakhanderoo7@gmail.com>
2025-05-31 19:37:04 +0330

    refactor methode name

--- src/main/java/com/example/maktabsharif/homeservices/service/SpecialistService.java
+++ src/main/java/com/example/maktabsharif/homeservices/service/SpecialistService.java
@@ -1,5 +1,6 @@
 package com.example.maktabsharif.homeservices.service;
 
+import com.example.maktabsharif.homeservices.dto.user.SpecialistCreateDTO;
 import com.example.maktabsharif.homeservices.dto.user.UserCreateDTO;
 import com.example.maktabsharif.homeservices.dto.user.UserDTO;
 import com.example.maktabsharif.homeservices.dto.user.UserUpdateDTO;
@@ -8,7 +9,7 @@ import com.example.maktabsharif.homeservices.entity.User;
 import java.io.IOException;
 
 public interface SpecialistService {
-    UserDTO savaSpecialist(UserCreateDTO createDTO) throws IOException;
+    UserDTO savaSpecialist(SpecialistCreateDTO createDTO) throws IOException;
     UserDTO updateSpecialist(UserUpdateDTO updateDTO) throws IOException;
     UserDTO findById(Long id);
     void deleteById(Long id);
